1dcc97791df3ee79957053c008bb7eaf8304db4e
Jhon <jhon.dev.silva@gmail.com>
2021-06-29 15:59:16 -0300

    uptade script.js

--- Bin2Dec/script.js
+++ Bin2Dec/script.js
@@ -2,13 +2,13 @@
 numero binario digitado no input bin e converter para 
 decimal e incluir o resultado dentro do input dec */
 function convert() {
-    let bin = Number(document.querySelector('#bin').value)
+    let bin = document.querySelector('#bin')
     let dec = document.querySelector('#dec')
 
-    if ((bin === '')) {
+    if ((bin.value === '')) {
         alert('Por favor, informe o número 0 ou 1')
     } else {
-        const convert = parseInt(bin, 2)
+        const convert = parseInt((bin.value), 2)
         if (isNaN(convert)) {
             alert('Por favor, informe o número 0 ou 1')
             dec.value = ''
